8c3ff796a03f62c89eff2391e375115de4a158ff
Tiago de Assis Santos <tstos.82@gmail.com>
2014-01-14 22:14:04 -0200

    Preparação para Github

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,7 @@ Imobil::Application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :user_name            => 'contato.ekarros@gmail.com',
+    :user_name            => 'jobber@cicladestec.com.br',
     :password             => '*tjdniq#',
     :authentication       => 'plain',
     :enable_starttls_auto => true  }
